cec62f5fea207591573e7b521bf069f9832e085a
TheSas99 <TheSas99@users.noreply.github.com>
2024-06-15 16:44:41 +0200

    some directory fixes

--- Assets/Scripts/Filters/ProductFilter.cs
+++ Assets/Scripts/Filters/ProductFilter.cs
@@ -36,103 +36,110 @@ public class ProductFilter : MonoBehaviour
 
     void InitializeProducts()
     {
-        // Example initialization with correct casting
-        allProducts.Add(new Product
+        allProducts = new List<Product>
         {
-            Name = "KYO98E",
-            Shape = "Rond",
-            Material = "Metaal",
-            Color = "Bruin",
-            Brand = "Kansai",
-            Gender = "Unisex",
-            Type = "Bril",
-            Price = 100,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/KYO98E/KYO98E")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "Product 2",
-            Shape = "Vijfhoek",
-            Material = "Metaal",
-            Color = "Goud",
-            Brand = "EPOS",
-            Gender = "Unisex",
-            Type = "Bril",
-            Price = 50,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/PRODUCT_2")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "Rayban 24019/LE",
-            Shape = "Ovaal",
-            Material = "Kunststof",
-            Color = "Blauw",
-            Brand = "Rayban",
-            Gender = "Unisex",
-            Type = "Bril",
-            Price = 150,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/Rayban_24019LE")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "2314Z10911",
-            Shape = "Ovaal",
-            Material = "Kunststof",
-            Color = "Rood",
-            Brand = "Rayban",
-            Gender = "Unisex",
-            Type = "Zonnebril",
-            Price = 237,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/2314Z10911")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "Tom Tailor",
-            Shape = "Rond",
-            Material = "Metaal",
-            Color = "Bruin",
-            Brand = "Tom Tailor",
-            Gender = "Unisex",
-            Type = "Bril",
-            Price = 237,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/Tom_Tailor")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "OfarKids MO0108B",
-            Shape = "Ovaal",
-            Material = "Kunststof",
-            Brand = "Ofar",
-            Gender = "Kinderen",
-            Color = "Paars",
-            Type = "Sportbril",
-            Price = 65,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/OfarKids_MO0108B")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "Ofar Blue Glasses",
-            Shape = "Ovaal",
-            Material = "Metaal",
-            Color = "Blauw",
-            Brand = "Ofar",
-            Gender = "Mannen",
-            Type = "Sportbril",
-            Price = 129,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/Ofar_blue_glasses")
-        });
-        allProducts.Add(new Product
-        {
-            Name = "Puma Bril",
-            Shape = "Ovaal",
-            Material = "Kunststof",
-            Color = "Zwart",
-            Brand = "Puma",
-            Gender = "Mannen",
-            Type = "Bril",
-            Price = 85,
-            Image = Resources.Load<Sprite>("Sprites/Images/Brillen/Puma_Bril_Zwart")
-        });
+            new Product
+            {
+                Name = "KYO98E",
+                Shape = "Rond",
+                Material = "Metaal",
+                Color = "Bruin",
+                Brand = "Kansai",
+                Gender = "Unisex",
+                Type = "Bril",
+                Price = 100,
+                Image = LoadProductImage("KYO98E")
+            },
+            new Product
+            {
+                Name = "Product 2",
+                Shape = "Vijfhoek",
+                Material = "Metaal",
+                Color = "Goud",
+                Brand = "EPOS",
+                Gender = "Unisex",
+                Type = "Bril",
+                Price = 50,
+                Image = LoadProductImage("PRODUCT_2")
+            },
+            new Product
+            {
+                Name = "Rayban 24019/LE",
+                Shape = "Ovaal",
+                Material = "Kunststof",
+                Color = "Blauw",
+                Brand = "Rayban",
+                Gender = "Unisex",
+                Type = "Bril",
+                Price = 150,
+                Image = LoadProductImage("Rayban_24019LE")
+            },
+            new Product
+            {
+                Name = "2314Z10911",
+                Shape = "Ovaal",
+                Material = "Kunststof",
+                Color = "Rood",
+                Brand = "Rayban",
+                Gender = "Unisex",
+                Type = "Zonnebril",
+                Price = 237,
+                Image = LoadProductImage("2314Z10911")
+            },
+            new Product
+            {
+                Name = "Tom Tailor",
+                Shape = "Rond",
+                Material = "Metaal",
+                Color = "Bruin",
+                Brand = "Tom Tailor",
+                Gender = "Unisex",
+                Type = "Bril",
+                Price = 237,
+                Image = LoadProductImage("Tom_Tailor")
+            },
+            new Product
+            {
+                Name = "OfarKids MO0108B",
+                Shape = "Ovaal",
+                Material = "Kunststof",
+                Color = "Paars",
+                Brand = "Ofar",
+                Gender = "Kinderen",
+                Type = "Sportbril",
+                Price = 65,
+                Image = LoadProductImage("OfarKids_MO0108B")
+            },
+            new Product
+            {
+                Name = "Ofar Blue Glasses",
+                Shape = "Ovaal",
+                Material = "Metaal",
+                Color = "Blauw",
+                Brand = "Ofar",
+                Gender = "Mannen",
+                Type = "Sportbril",
+                Price = 129,
+                Image = LoadProductImage("Ofar_blue_glasses")
+            },
+            new Product
+            {
+                Name = "Puma Bril",
+                Shape = "Ovaal",
+                Material = "Kunststof",
+                Color = "Zwart",
+                Brand = "Puma",
+                Gender = "Mannen",
+                Type = "Bril",
+                Price = 85,
+                Image = LoadProductImage("Puma_Bril_Zwart")
+            }
+        };
+    }
+
+    Sprite LoadProductImage(string imageName)
+    {
+        return Resources.Load<Sprite>($"Sprites/Images/Brillen/{imageName}");
     }
 
     void ApplyFilters()
@@ -145,29 +152,40 @@ public class ProductFilter : MonoBehaviour
         string selectedGender = genderDropdown.options[genderDropdown.value].text;
 
         SaveFilters(selectedShape, selectedMaterial, selectedColor, selectedType, selectedBrand, selectedGender);
+        filteredProducts = FilterProducts(selectedShape, selectedMaterial, selectedColor, selectedType, selectedBrand, selectedGender);
+        UpdateProductDisplay();
+    }
 
-        filteredProducts.Clear();
+    List<Product> FilterProducts(string shape, string material, string color, string type, string brand, string gender)
+    {
+        List<Product> result = new List<Product>();
 
         foreach (var product in allProducts)
         {
-            bool shapeMatch = selectedShape == "All" || selectedShape == "GEEN KEUZE" || product.Shape == selectedShape;
-            bool materialMatch = selectedMaterial == "All" || selectedMaterial == "GEEN KEUZE" || product.Material == selectedMaterial;
-            bool colorMatch = selectedColor == "All" || selectedColor == "GEEN KEUZE" || product.Color == selectedColor;
-            bool typeMatch = selectedType == "All" || selectedType == "GEEN KEUZE" || product.Type == selectedType;
-            bool brandMatch = selectedBrand == "All" || selectedBrand == "GEEN KEUZE" || product.Brand == selectedBrand;
-
-            // Adjust gender match logic
-            bool genderMatch = selectedGender == "All" || selectedGender == "GEEN KEUZE" ||
-                               product.Gender == selectedGender || product.Gender == "Unisex" &&
-                               (selectedGender == "Mannen" || selectedGender == "Vrouwen" || selectedGender == "Unisex");
-
-            if (shapeMatch && materialMatch && colorMatch && typeMatch && brandMatch && genderMatch)
+            if (MatchesFilter(product.Shape, shape) &&
+                MatchesFilter(product.Material, material) &&
+                MatchesFilter(product.Color, color) &&
+                MatchesFilter(product.Type, type) &&
+                MatchesFilter(product.Brand, brand) &&
+                MatchesGenderFilter(product.Gender, gender))
             {
-                filteredProducts.Add(product);
+                result.Add(product);
             }
         }
 
-        UpdateProductDisplay();
+        return result;
+    }
+
+    bool MatchesFilter(string productAttribute, string selectedFilter)
+    {
+        return selectedFilter == "All" || selectedFilter == "GEEN KEUZE" || productAttribute == selectedFilter;
+    }
+
+    bool MatchesGenderFilter(string productGender, string selectedGender)
+    {
+        return selectedGender == "All" || selectedGender == "GEEN KEUZE" ||
+               productGender == selectedGender ||
+               productGender == "Unisex" && (selectedGender == "Mannen" || selectedGender == "Vrouwen" || selectedGender == "Unisex");
     }
 
     void UpdateProductDisplay()
@@ -180,7 +198,6 @@ public class ProductFilter : MonoBehaviour
         foreach (var product in filteredProducts)
         {
             GameObject productGO = Instantiate(productPrefab, productContainer);
-
             ProductButton productButton = productGO.GetComponentInChildren<ProductButton>();
 
             if (productButton != null)
@@ -206,11 +223,24 @@ public class ProductFilter : MonoBehaviour
 
     void LoadFilters()
     {
-        if (PlayerPrefs.HasKey(ShapePrefKey)) shapeDropdown.value = shapeDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(ShapePrefKey));
-        if (PlayerPrefs.HasKey(MaterialPrefKey)) materialDropdown.value = materialDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(MaterialPrefKey));
-        if (PlayerPrefs.HasKey(ColorPrefKey)) colorDropdown.value = colorDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(ColorPrefKey));
-        if (PlayerPrefs.HasKey(TypePrefKey)) typeDropdown.value = typeDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(TypePrefKey));
-        if (PlayerPrefs.HasKey(BrandPrefKey)) brandDropdown.value = brandDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(BrandPrefKey));
-        if (PlayerPrefs.HasKey(GenderPrefKey)) genderDropdown.value = genderDropdown.options.FindIndex(option => option.text == PlayerPrefs.GetString(GenderPrefKey));
+        SetDropdownValue(shapeDropdown, ShapePrefKey);
+        SetDropdownValue(materialDropdown, MaterialPrefKey);
+        SetDropdownValue(colorDropdown, ColorPrefKey);
+        SetDropdownValue(typeDropdown, TypePrefKey);
+        SetDropdownValue(brandDropdown, BrandPrefKey);
+        SetDropdownValue(genderDropdown, GenderPrefKey);
+    }
+
+    void SetDropdownValue(TMP_Dropdown dropdown, string prefKey)
+    {
+        if (PlayerPrefs.HasKey(prefKey))
+        {
+            string savedValue = PlayerPrefs.GetString(prefKey);
+            int index = dropdown.options.FindIndex(option => option.text == savedValue);
+            if (index >= 0)
+            {
+                dropdown.value = index;
+            }
+        }
     }
 }
