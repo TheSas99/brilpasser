3dad51eef61e43d7b795663821f049eb5e3940cb
TheSas99 <TheSas99@users.noreply.github.com>
2024-06-25 15:37:59 +0200

    Fixed text

--- Assets/Scripts/Filters/ProductUI.cs
+++ Assets/Scripts/Filters/ProductUI.cs
@@ -11,6 +11,8 @@ public class ProductUI : MonoBehaviour
     public TMP_Text productPriceText;
     public TMP_Text productTypeText;
     public TMP_Text productColorText;
+    public TMP_Text productBrandText;
+    public TMP_Text productGenderText;
     public Image productImage;
     public Button closeButton;
     public Button continueButton;
@@ -23,12 +25,14 @@ public class ProductUI : MonoBehaviour
     public void Setup(Product product)
     {
         productNameText.text = product.Name;
-        productShapeText.text = product.Shape;
-        productMaterialText.text = product.Material;
+        productShapeText.text = "Vorm: \n" + product.Shape;
+        productMaterialText.text = "Materiaal: \n" + product.Material;
         productPriceText.text = "€" + product.Price.ToString("F2");
-        productTypeText.text = product.Type;
-        productColorText.text = product.Color;
+        productTypeText.text = "Type: \n" + product.Type;
+        productColorText.text = "Kleur: \n" + product.Color;
         productImage.sprite = product.Image;
+        productBrandText.text = "Merk: \n" + product.Brand;
+        productGenderText.text = "Gender: \n" + product.Gender;
 
         Debug.Log("IsPasbaar: " + product.IsPasbaar);
 
